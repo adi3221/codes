811456fac60660dfdeea22b8a4f352116a90cf1c
adi3221 <atiwari18681868@gmail.com>
2022-02-22 19:41:16 +0530

    coooolllllllllmmmm

--- 1toNprint.cpp
+++ 1toNprint.cpp
@@ -3,8 +3,8 @@ using namespace std;
 
 void solve(int n){
     if(n==1){
-        cout<< 1<<" ";
-        return ;
+        cout<< 1<<" ";        
+        return ;  
     }
     cout<<n<< " ";
     solve(n-1);
